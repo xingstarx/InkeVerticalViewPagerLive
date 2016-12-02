b7b71f9a5470f8185c1e138a301104ba4ec45267
xingxing <xxx823952375@gmail.com>
2016-12-03 00:36:03 +0800

    init commit

---
+++ app/src/main/java/com/star/inke/MainActivity.java
@@ -0,0 +1,246 @@
+package com.star.inke;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.pili.pldroid.player.AVOptions;
+import com.pili.pldroid.player.PLMediaPlayer;
+import com.pili.pldroid.player.widget.PLVideoTextureView;
+import com.star.inke.utils.Utils;
+import com.star.inke.widget.MediaController;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int MESSAGE_ID_RECONNECTING = 0x01;
+
+    private MediaController mMediaController;
+    private PLVideoTextureView mVideoView;
+    private Toast mToast = null;
+    private String mVideoPath = null;
+    private int mRotation = 0;
+    private int mDisplayAspectRatio = PLVideoTextureView.ASPECT_RATIO_FIT_PARENT; //default
+    private View mLoadingView;
+    private View mCoverView = null;
+    private boolean mIsActivityPaused = true;
+    private static final String DEFAULT_TEST_URL = "rtmp://live.hkstv.hk.lxdns.com/live/hks";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mVideoView = (PLVideoTextureView) findViewById(R.id.VideoView);
+        mLoadingView = findViewById(R.id.LoadingView);
+        mVideoView.setBufferingIndicator(mLoadingView);
+        mLoadingView.setVisibility(View.VISIBLE);
+        mCoverView = findViewById(R.id.CoverView);
+        mVideoView.setCoverView(mCoverView);
+
+        mVideoPath = getIntent().getStringExtra("videoPath");
+
+        // If you want to fix display orientation such as landscape, you can use the code show as follow
+        //
+        // if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        //     mVideoView.setPreviewOrientation(0);
+        // }
+        // else if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+        //     mVideoView.setPreviewOrientation(270);
+        // }
+
+        mVideoPath = getIntent().getStringExtra("videoPath");
+        mVideoPath = DEFAULT_TEST_URL;
+
+        AVOptions options = new AVOptions();
+
+        int isLiveStreaming = getIntent().getIntExtra("liveStreaming", 1);
+        // the unit of timeout is ms
+        options.setInteger(AVOptions.KEY_PREPARE_TIMEOUT, 10 * 1000);
+        options.setInteger(AVOptions.KEY_GET_AV_FRAME_TIMEOUT, 10 * 1000);
+        // Some optimization with buffering mechanism when be set to 1
+        options.setInteger(AVOptions.KEY_LIVE_STREAMING, isLiveStreaming);
+        if (isLiveStreaming == 1) {
+            options.setInteger(AVOptions.KEY_DELAY_OPTIMIZATION, 1);
+        }
+
+        // 1 -> hw codec enable, 0 -> disable [recommended]
+        int codec = getIntent().getIntExtra("mediaCodec", 0);
+        options.setInteger(AVOptions.KEY_MEDIACODEC, codec);
+
+        // whether start play automatically after prepared, default value is 1
+        options.setInteger(AVOptions.KEY_START_ON_PREPARED, 0);
+
+        mVideoView.setAVOptions(options);
+
+        // You can mirror the display
+        // mVideoView.setMirror(true);
+
+        // You can also use a custom `MediaController` widget
+        mMediaController = new MediaController(this, false, isLiveStreaming == 1);
+        mVideoView.setMediaController(mMediaController);
+
+        mVideoView.setOnCompletionListener(mOnCompletionListener);
+        mVideoView.setOnErrorListener(mOnErrorListener);
+
+        mVideoView.setVideoPath(mVideoPath);
+        mVideoView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mToast = null;
+        mVideoView.pause();
+        mIsActivityPaused = true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mIsActivityPaused = false;
+        mVideoView.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mVideoView.stopPlayback();
+    }
+
+    public void onClickRotate(View v) {
+        mRotation = (mRotation + 90) % 360;
+        mVideoView.setDisplayOrientation(mRotation);
+    }
+
+    public void onClickSwitchScreen(View v) {
+        mDisplayAspectRatio = (mDisplayAspectRatio + 1) % 5;
+        mVideoView.setDisplayAspectRatio(mDisplayAspectRatio);
+        switch (mVideoView.getDisplayAspectRatio()) {
+            case PLVideoTextureView.ASPECT_RATIO_ORIGIN:
+                showToastTips("Origin mode");
+                break;
+            case PLVideoTextureView.ASPECT_RATIO_FIT_PARENT:
+                showToastTips("Fit parent !");
+                break;
+            case PLVideoTextureView.ASPECT_RATIO_PAVED_PARENT:
+                showToastTips("Paved parent !");
+                break;
+            case PLVideoTextureView.ASPECT_RATIO_16_9:
+                showToastTips("16 : 9 !");
+                break;
+            case PLVideoTextureView.ASPECT_RATIO_4_3:
+                showToastTips("4 : 3 !");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private PLMediaPlayer.OnErrorListener mOnErrorListener = new PLMediaPlayer.OnErrorListener() {
+        @Override
+        public boolean onError(PLMediaPlayer mp, int errorCode) {
+            boolean isNeedReconnect = false;
+            switch (errorCode) {
+                case PLMediaPlayer.ERROR_CODE_INVALID_URI:
+                    showToastTips("Invalid URL !");
+                    break;
+                case PLMediaPlayer.ERROR_CODE_404_NOT_FOUND:
+                    showToastTips("404 resource not found !");
+                    break;
+                case PLMediaPlayer.ERROR_CODE_CONNECTION_REFUSED:
+                    showToastTips("Connection refused !");
+                    break;
+                case PLMediaPlayer.ERROR_CODE_CONNECTION_TIMEOUT:
+                    showToastTips("Connection timeout !");
+                    isNeedReconnect = true;
+                    break;
+                case PLMediaPlayer.ERROR_CODE_EMPTY_PLAYLIST:
+                    showToastTips("Empty playlist !");
+                    break;
+                case PLMediaPlayer.ERROR_CODE_STREAM_DISCONNECTED:
+                    showToastTips("Stream disconnected !");
+                    isNeedReconnect = true;
+                    break;
+                case PLMediaPlayer.ERROR_CODE_IO_ERROR:
+                    showToastTips("Network IO Error !");
+                    isNeedReconnect = true;
+                    break;
+                case PLMediaPlayer.ERROR_CODE_UNAUTHORIZED:
+                    showToastTips("Unauthorized Error !");
+                    break;
+                case PLMediaPlayer.ERROR_CODE_PREPARE_TIMEOUT:
+                    showToastTips("Prepare timeout !");
+                    isNeedReconnect = true;
+                    break;
+                case PLMediaPlayer.ERROR_CODE_READ_FRAME_TIMEOUT:
+                    showToastTips("Read frame timeout !");
+                    isNeedReconnect = true;
+                    break;
+                case PLMediaPlayer.MEDIA_ERROR_UNKNOWN:
+                    break;
+                default:
+                    showToastTips("unknown error !");
+                    break;
+            }
+            // Todo pls handle the error status here, reconnect or call finish()
+            if (isNeedReconnect) {
+                sendReconnectMessage();
+            } else {
+                finish();
+            }
+            // Return true means the error has been handled
+            // If return false, then `onCompletion` will be called
+            return true;
+        }
+    };
+
+    private PLMediaPlayer.OnCompletionListener mOnCompletionListener = new PLMediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(PLMediaPlayer plMediaPlayer) {
+            showToastTips("Play Completed !");
+            finish();
+        }
+    };
+
+    private void showToastTips(final String tips) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mToast != null) {
+                    mToast.cancel();
+                }
+                mToast = Toast.makeText(MainActivity.this, tips, Toast.LENGTH_SHORT);
+                mToast.show();
+            }
+        });
+    }
+
+    private void sendReconnectMessage() {
+        showToastTips("正在重连...");
+        mLoadingView.setVisibility(View.VISIBLE);
+        mHandler.removeCallbacksAndMessages(null);
+        mHandler.sendMessageDelayed(mHandler.obtainMessage(MESSAGE_ID_RECONNECTING), 500);
+    }
+
+    protected Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what != MESSAGE_ID_RECONNECTING) {
+                return;
+            }
+            if (mIsActivityPaused || !Utils.isLiveStreamingAvailable()) {
+                finish();
+                return;
+            }
+            if (!Utils.isNetworkAvailable(MainActivity.this)) {
+                sendReconnectMessage();
+                return;
+            }
+            mVideoView.setVideoPath(mVideoPath);
+            mVideoView.start();
+        }
+    };
+}

a44478da1293fcdc00d7a53d66f7fdbf697bdc4d
xingxing <xxx823952375@gmail.com>
2016-12-03 18:44:33 +0800

    添加竖直方向的viewPager ，实现仿映客滑动切换视频的效果

--- app/src/main/java/com/star/inke/MainActivity.java
+++ app/src/main/java/com/star/inke/MainActivity.java
@@ -4,8 +4,16 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.pili.pldroid.player.AVOptions;
@@ -14,9 +22,20 @@ import com.pili.pldroid.player.widget.PLVideoTextureView;
 import com.star.inke.utils.Utils;
 import com.star.inke.widget.MediaController;
 
+import java.util.ArrayList;
+
+import fr.castorflex.android.verticalviewpager.VerticalViewPager;
+import rx.Observable;
+import rx.Subscription;
+import rx.android.app.AppObservable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.Subscriptions;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int MESSAGE_ID_RECONNECTING = 0x01;
+    public static final String TAG = "MainActivity";
 
     private MediaController mMediaController;
     private PLVideoTextureView mVideoView;
@@ -24,58 +43,115 @@ public class MainActivity extends AppCompatActivity {
     private String mVideoPath = null;
     private int mRotation = 0;
     private int mDisplayAspectRatio = PLVideoTextureView.ASPECT_RATIO_PAVED_PARENT;
-    private View mLoadingView;
-    private View mCoverView = null;
     private static final String DEFAULT_TEST_URL = "http://live.hkstv.hk.lxdns.com/live/hks/playlist.m3u8";
     //        private static final String DEFAULT_TEST_URL = "rtmp://live.hkstv.hk.lxdns.com/live/hks";
 //    private static final String DEFAULT_TEST_URL = "http://mobile.xinhuashixun.com/Live/cncHD.m3u8";
     private boolean mIsActivityPaused = true;
+    private VerticalViewPager mViewPager;
+
+    private RelativeLayout mRoomContainer;
+    private PagerAdapter mPagerAdapter;
+    private int mCurrentItem;
+    private int isLiveStreaming = 1;
+    private AVOptions options;
+    private FrameLayout mFragmentContainer;
+    private ArrayList<String> mVideoUrls = new ArrayList<>();
+    private Subscription mSubscription = Subscriptions.empty();
+    private FragmentManager mFragmentManager;
+    private int mRoomId = -1;
+    private RoomFragment mRoomFragment = RoomFragment.newInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mVideoView = (PLVideoTextureView) findViewById(R.id.VideoView);
-        mLoadingView = findViewById(R.id.LoadingView);
-        mVideoView.setBufferingIndicator(mLoadingView);
-        mLoadingView.setVisibility(View.VISIBLE);
-        mCoverView = findViewById(R.id.CoverView);
-        mVideoView.setCoverView(mCoverView);
+        setContentView(R.layout.activity_main_2);
+        mViewPager = (VerticalViewPager) findViewById(R.id.view_pager);
+        mRoomContainer = (RelativeLayout) LayoutInflater.from(this).inflate(R.layout.view_room_container, null);
+        mFragmentContainer = (FrameLayout) mRoomContainer.findViewById(R.id.fragment_container);
+        mVideoView = (PLVideoTextureView) mRoomContainer.findViewById(R.id.texture_view);
         mVideoView.setDisplayAspectRatio(mDisplayAspectRatio);
-        mVideoPath = getIntent().getStringExtra("videoPath");
         mVideoPath = DEFAULT_TEST_URL;
-        AVOptions options = new AVOptions();
+        mFragmentManager = getSupportFragmentManager();
+        initAVOptions();
+        mVideoView.setAVOptions(options);
+        mMediaController = new MediaController(this, false, true);
+        generateUrls();
+        mPagerAdapter = new PagerAdapter();
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Log.e(TAG, "mCurrentId == " + position + ", positionOffset == " + positionOffset +
+                        ", positionOffsetPixels == " + positionOffsetPixels);
+                mCurrentItem = position;
+            }
+        });
 
-        int isLiveStreaming = getIntent().getIntExtra("liveStreaming", 1);
+        mViewPager.setPageTransformer(false, new ViewPager.PageTransformer() {
+            @Override
+            public void transformPage(View page, float position) {
+                ViewGroup viewGroup = (ViewGroup) page;
+                Log.e(TAG, "page.id == " + page.getId() + ", position == " + position);
+
+                if ((position < 0 && viewGroup.getId() == mCurrentItem) || (position >= 1 && viewGroup.getId() == mCurrentItem)) {
+                    if (mRoomContainer.getParent() != null && mRoomContainer.getParent() instanceof ViewGroup) {
+                        ((ViewGroup) (mRoomContainer.getParent())).removeView(mRoomContainer);
+                    }
+                }
+                // 满足此种条件，表明需要加载直播视频，以及聊天室了
+                if (viewGroup.getId() == mCurrentItem && position == 0 && mCurrentItem != mRoomId) {
+                    if (mRoomContainer.getParent() != null && mRoomContainer.getParent() instanceof ViewGroup) {
+                        ((ViewGroup) (mRoomContainer.getParent())).removeView(mRoomContainer);
+                    }
+                    loadVideoAndChatRoom(viewGroup, mCurrentItem);
+                }
+            }
+        });
+        mViewPager.setAdapter(mPagerAdapter);
+    }
+
+    private void generateUrls() {
+        for (int i = 0; i < 10; i++) {
+            mVideoUrls.add(DEFAULT_TEST_URL);
+        }
+    }
+
+    private void initAVOptions() {
+        options = new AVOptions();
         // the unit of timeout is ms
         options.setInteger(AVOptions.KEY_PREPARE_TIMEOUT, 10 * 1000);
         options.setInteger(AVOptions.KEY_GET_AV_FRAME_TIMEOUT, 10 * 1000);
         // Some optimization with buffering mechanism when be set to 1
         options.setInteger(AVOptions.KEY_LIVE_STREAMING, isLiveStreaming);
-        if (isLiveStreaming == 1) {
-            options.setInteger(AVOptions.KEY_DELAY_OPTIMIZATION, 1);
-        }
-
+        options.setInteger(AVOptions.KEY_DELAY_OPTIMIZATION, 1);
         // 1 -> hw codec enable, 0 -> disable [recommended]
-        int codec = getIntent().getIntExtra("mediaCodec", 0);
+        int codec = 0;
         options.setInteger(AVOptions.KEY_MEDIACODEC, codec);
-
         // whether start play automatically after prepared, default value is 1
         options.setInteger(AVOptions.KEY_START_ON_PREPARED, 0);
+    }
 
-        mVideoView.setAVOptions(options);
+    private boolean mInit = false;
+    /**
+     * @param viewGroup
+     * @param currentItem
+     */
+    private void loadVideoAndChatRoom(ViewGroup viewGroup, int currentItem) {
+//        mSubscription = AppObservable.bindActivity(this, ).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).
+        if (!mInit) {
+            mFragmentManager.beginTransaction().add(mFragmentContainer.getId(), mRoomFragment).commitAllowingStateLoss();
+            mInit = true;
+        }
+        loadVideo(currentItem);
+        viewGroup.addView(mRoomContainer);
+        mRoomId = currentItem;
+    }
 
-        // You can mirror the display
-        // mVideoView.setMirror(true);
-
-        // You can also use a custom `MediaController` widget
-        mMediaController = new MediaController(this, false, isLiveStreaming == 1);
+    private void loadVideo(int position) {
         mVideoView.setMediaController(mMediaController);
-
         mVideoView.setOnCompletionListener(mOnCompletionListener);
         mVideoView.setOnErrorListener(mOnErrorListener);
-
-        mVideoView.setVideoPath(mVideoPath);
+        mVideoView.setVideoPath(mVideoUrls.get(position));
         mVideoView.start();
     }
 
@@ -98,6 +174,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mVideoView.stopPlayback();
+        mSubscription.unsubscribe();
     }
 
     private PLMediaPlayer.OnErrorListener mOnErrorListener = new PLMediaPlayer.OnErrorListener() {
@@ -181,7 +258,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void sendReconnectMessage() {
         showToastTips("正在重连...");
-        mLoadingView.setVisibility(View.VISIBLE);
         mHandler.removeCallbacksAndMessages(null);
         mHandler.sendMessageDelayed(mHandler.obtainMessage(MESSAGE_ID_RECONNECTING), 500);
     }
@@ -204,4 +280,31 @@ public class MainActivity extends AppCompatActivity {
             mVideoView.start();
         }
     };
+
+
+    class PagerAdapter extends android.support.v4.view.PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return mVideoUrls.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View view = LayoutInflater.from(container.getContext()).inflate(R.layout.view_room_item, null);
+            view.setId(position);
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(container.findViewById(position));
+        }
+    }
 }
---
+++ app/src/main/java/com/star/inke/RoomFragment.java
@@ -0,0 +1,30 @@
+package com.star.inke;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by xiongxingxing on 16/12/3.
+ */
+
+public class RoomFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_room, container, false);
+        return view;
+    }
+
+    public static RoomFragment newInstance() {
+        Bundle args = new Bundle();
+        RoomFragment fragment = new RoomFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+}
+

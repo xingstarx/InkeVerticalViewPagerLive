b7f8e91ef80bc8d6bab529640f7876d80dbe4848
xingxing <xxx823952375@gmail.com>
2016-12-04 00:25:40 +0800

    简单模拟初始化聊天室

--- app/src/main/java/com/star/inke/RoomFragment.java
+++ app/src/main/java/com/star/inke/RoomFragment.java
@@ -6,12 +6,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Locale;
+import java.util.Random;
 
 /**
  * Created by xiongxingxing on 16/12/3.
  */
 
 public class RoomFragment extends Fragment {
+
+    private Button mButton;
+    private Random random = new Random();
+
+    public static RoomFragment newInstance() {
+        Bundle args = new Bundle();
+        RoomFragment fragment = new RoomFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Nullable
     @Override
@@ -20,11 +34,12 @@ public class RoomFragment extends Fragment {
         return view;
     }
 
-    public static RoomFragment newInstance() {
-        Bundle args = new Bundle();
-        RoomFragment fragment = new RoomFragment();
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        mButton = (Button) view.findViewById(R.id.btn_manage);
+        mButton.setText(String.format(Locale.getDefault(), "映客号: %s", "103209" + random.nextInt(10)));
     }
+
+
 }
 
